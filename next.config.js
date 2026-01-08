8a05db4b8eeb8ff729bf053ee2e74d48a7c2fd3b
Yap Jia Hong <yapjiahong-p@proton.me>
2026-01-08 12:29:31 +0800

    Removed basePath and assetPath

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
 const nextConfig = {
   output: "export",
   trailingSlash: true,
-  basePath: "/paws-and-preferences",
-  assetPrefix: "/paws-and-preferences/",
   images: {
     unoptimized: true,
   },
